4c03a0d26efe6f2f2f17404f383d688cda2f0bbc
Aaron Tao <aarontao@aarontao-fedora-it2212249.DHCP.thefacebook.com>
2018-12-07 17:10:16 -0800

    embed json in exe through cmake

--- staticmember/helloworld.cpp
+++ staticmember/helloworld.cpp
@@ -4,39 +4,19 @@
 #include <exception>
 #include <regex>
 
+extern char _binary_AV2_json_start;
+extern char _binary_AV2_json_end;
+extern const int _binary_AV2_json_size;
+
 using namespace std;
-
-class tracker
-{
-  public:
-    static map<string, string> MsgMap;
-
-    static string replaceString(string oldstring);
-};
-
-map<string, string> tracker::MsgMap = {
-    {"Not able to find ov580 interface with serial number.*",
-     "please re-plug your USB cable"}};
-
-string tracker::replaceString(string oldstring)
-{
-    for (auto item : MsgMap)
-    {
-        regex expr(item.first);
-        if (regex_match(oldstring, expr))
-            return item.second;
-    }
-    return oldstring;
-};
 
 int main()
 
 {
-    string teststring("Not able to find ov580 interface with serial number 0072510f14170807050000022c2400010072510f14170807050000031f1e0001s");
+    char *p = &_binary_AV2_json_start;
 
-    string resultstring = tracker::replaceString(teststring);
+    while (p != &_binary_AV2_json_end)
+        putchar(*p++);
 
-    string teststring2("222Not able to find ov580 interface with serial number 0072510f14170807050000022c2400010072510f14170807050000031f1e0001s");
-    string resultstring2 = tracker::replaceString(teststring2);
     return 0;
 }
